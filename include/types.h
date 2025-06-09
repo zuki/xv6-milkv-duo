2e1be5b1fb7c1ca3eaf0afb0bad0b6c4265542d1
Keiji Suzuki <zuki.ebetsu@gmail.com>
2025-06-09 10:24:05 +0900

    linuxで使われているintの型定義を追加

--- include/types.h
+++ include/types.h
@@ -21,6 +21,15 @@ typedef int         int32_t;
 typedef long        int64_t;
 typedef long        ssize_t;
 
+typedef uint8_t     __u8;
+typedef int8_t      __s8;
+typedef uint16_t    __u16;
+typedef int16_t     __s16;
+typedef uint32_t    __u32;
+typedef int32_t     __s32;
+typedef uint64_t    __u64;
+typedef int64_t     __s64;
+
 typedef uint64_t    pde_t;
 typedef int         boot;
 typedef int         pid_t;
